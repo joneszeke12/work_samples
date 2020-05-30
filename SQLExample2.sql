3e47e864d67918ae76e2ede8c39d5283f0914985
joneszeke12 <50714250+joneszeke12@users.noreply.github.com>
2020-05-30 10:17:02 -0400

    .
    .

--- SQLExample2.sql
+++ SQLExample2.sql
@@ -1,26 +1,48 @@
-/*A stored procedure that creates a fact table with column names coming from another table.*/
+CREATE TABLE [dbo].[KV] (
+ [ID] int
+,[Key] varchar(32)
+,[Value] int
+);
+GO
 
-ALTER PROCEDURE [utility].[usp_CreateSurveyTable]
+INSERT INTO 
+	[dbo].[KV] 
+VALUES
+	 (1,'KEY1',1)
+	,(2,'KEY1',2)
+	,(3,'KEY2',10)
+	,(4,'KEY2',20)
+	,(5,'KEY2',30)
+	,(6,'KEY3',6)
+	,(7,'KEY3',12);
+GO
+
+/*
+A stored procedure that creates a table dynamically with a unique identifier and columns of the integer data type.
+I've used this type of procedure a few times to create fact tables with hundreds of foreign key columns.
+*/
+CREATE PROCEDURE [dbo].[usp_CreateDynamicTable]
 AS
 BEGIN
 	DECLARE @sql nvarchar(MAX) = 
-	'/*CREATE TABLE [base].[Survey] (
-		%s
+	'/*CREATE TABLE [dbo].[Table] (
+		 ID int
+		,%s
 	 );*/'
 	DECLARE @fields nvarchar(MAX) = 
 	STUFF(
 			(
 				SELECT DISTINCT 
-					[Variable] + ' int,'
+					',' + [KEY] + ' int'
 				FROM 
-					[base].QuestionMapping
+					[dbo].[KV]
 				FOR XML PATH(''), TYPE
 			).value('.','nvarchar(MAX)'),
 			1,
 			0,
 			''
 	);
-	SET @fields = LEFT(@fields,LEN(@fields) - 1);
+	SET @fields = RIGHT(@fields,LEN(@fields) - 1);
 	DECLARE @stmnt nvarchar(MAX) = REPLACE(@sql,'%s',@fields);
 	EXEC sp_executesql @stmnt;
 END
